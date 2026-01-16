1b3758c74af493da93a9f00cc95a7440709e525c
Ashokan K <ashokankuganathan@gmail.com>
2026-01-16 18:02:07 +0530

    updated config.js

--- config.ts
+++ config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://science-institute-backend.vercel.app';
